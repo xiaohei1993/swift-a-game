4a35ed48890e922e2a65220faff298797a03a6a5
xiaohei1993 <apple@zhanghongyideMacBook-Pro.local>
2015-02-09 11:00:54 +0800

    Initial Commit

---
+++ swift a game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift a game
+//
+//  Created by 张宏毅 on 15/2/9.
+//  Copyright (c) 2015年 zhy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
